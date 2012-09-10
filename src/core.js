91f1462dadda684c25c5386736efa5977e917d2c
Matt Priour <mpriour@opengeo.org>
2012-09-10 13:26:48 -0500

    Enable 'role' as a special property when used outside of the 'data' property. Adjust example.

--- src/core.js
+++ src/core.js
@@ -21,9 +21,14 @@
     };
     jmw.buildWidget = function(role, config) {
         var $el = null;
-        if (this.widgetBuilder && this.widgetBuilder['role']) {
+        if (this.WidgetBuilder && this.WidgetBuilder['role']) {
             $el = jmw.widgetBuilder['role'](config);
         } else {
+            if(role){
+                if(!config.data){ config.data={}; }
+                config.data.role = role;
+                delete config.role;
+            }
             $el = jmw.builder.build(config);
         }
         return $el;
@@ -37,55 +42,66 @@
         $.extend(this, options);
     };
     builder.prototype.build = function(elConfig) {
-            var el, data, $el;
-            el = document.createElement(elConfig.tag || jmw.builder.defaultTag);
-            $el = $(el);
+            var el, data, $el, complete = false;
 
-            data = elConfig.data || {};
-            var nsData = ($.mobile && $.mobile.ns) ? 'data-' + $.mobile.ns + '-' : 'data-';
-            $.each(data, function(key, val) {
-                //writes to the internal element / data cache
-                //but not to the actual element unless the attribute
-                //already exists
-                $el.jqmData(key, val);
-                //actually writes to the element
-                $el.attr(nsData+key, val);
-            });
+            if(elConfig.el){
+                $el = $(elConfig.el);
+                delete elConfig.el;
+            } else if (elConfig.role) {
+                $el = jmw.buildWidget(elConfig.role, elConfig);
+                complete = !!$el;
+            } else {
+                el = document.createElement(elConfig.tag || this.defaultTag);
+                $el = $(el);
+            }
 
-            delete elConfig.tag;
-            delete elConfig.data;
+            if(!complete){
+                data = elConfig.data || {};
+                var nsData = ($.mobile && $.mobile.ns) ? 'data-' + $.mobile.ns + '-' : 'data-';
+                $.each(data, function(key, val) {
+                    //writes to the internal element / data cache
+                    //but not to the actual element unless the attribute
+                    //already exists
+                    $el.jqmData(key, val);
+                    //actually writes to the element
+                    $el.attr(nsData+key, val);
+                });
 
-            $.each(elConfig, function(key, val) {
-                switch (key) {
-                case 'class':
-                    $el.addClass(val);
-                    break;
-                case 'text':
-                    $el.text(val);
-                    break;
-                case 'value':
-                    $el.val(val);
-                    break;
-                case 'html':
-                    $el.html(val);
-                    break;
-                case 'id':
-                    $el.attr('id', val);
-                    break;
-                case 'cn':
-                case 'children':
-                    $.each(val, function(i, val) {
-                        if (val.role) {
-                            $el.append(jmw.buildWidget(val.role, val));
-                        } else {
-                            $el.append(jmw.builder.build(val));
-                        }
-                    });
-                    break;
-                default:
-                    $el.attr(key, val);
-                }
-            });
+                delete elConfig.tag;
+                delete elConfig.data;
+
+                $.each(elConfig, function(key, val) {
+                    switch (key) {
+                    case 'class':
+                        $el.addClass(val);
+                        break;
+                    case 'text':
+                        $el.text(val);
+                        break;
+                    case 'value':
+                        $el.val(val);
+                        break;
+                    case 'html':
+                        $el.html(val);
+                        break;
+                    case 'id':
+                        $el.attr('id', val);
+                        break;
+                    case 'cn':
+                    case 'children':
+                        $.each(val, function(i, val) {
+                            if (val.role) {
+                                $el.append(jmw.buildWidget(val.role, val));
+                            } else {
+                                $el.append(jmw.builder.build(val));
+                            }
+                        });
+                        break;
+                    default:
+                        $el.attr(key, val);
+                    }
+                });
+            }
 
             return $el;
         };
