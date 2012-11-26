630d41e9f4d71f6843b86c232338655ecea8b929
Matt Priour <mpriour@opengeo.org>
2012-11-26 08:42:33 -0600

    add widget shortcut methods

--- src/core.js
+++ src/core.js
@@ -1,12 +1,28 @@
 (function(){
+    var root = this;
     var jmw = {};
+    // Export the jmw object for **Node.js**, with
+    // backwards-compatibility for the old `require()` API. If we're in
+    // the browser, add `jmw` as a global object via a string identifier,
+    // for Closure Compiler "advanced" mode.
+    if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = jmw;
+    }
+    exports.jmw = jmw;
+    } else {
+    root['jmw'] = jmw;
+    }
+
+    jmw.defaultTag = 'div';
+
     jmw.layout = function(el, config) {
         if(el && !config){
             config = el;
             el = $('body');
         }
         var $el = $(el);
-        var $layout = jmw.builder.build(config);
+        var $layout = jmw.build(config, config && config.role == 'page');
         el.append($layout);
         var role = $layout.jqmData('role');
         if(role && $layout[role]){
@@ -14,41 +30,42 @@
         } else {
             $layout.page();
         }
-        if (role == 'page') {
+        if (role == 'page' && config.defer !== true) {
             $.mobile.changePage($layout);
         }
         return $layout;
     };
     jmw.buildWidget = function(role, config) {
         var $el = null;
-        if (this.WidgetBuilder && this.WidgetBuilder['role']) {
-            $el = jmw.widgetBuilder['role'](config);
+        if (jmw.widget[role]) {
+            $el = jmw.widget[role](config);
         } else {
             if(role){
                 if(!config.data){ config.data={}; }
                 config.data.role = role;
                 delete config.role;
             }
-            $el = jmw.builder.build(config);
+            $el = jmw.build(config);
         }
         return $el;
     };
-    this.jmw = jmw;
-}());
-
-(function(jmw){
-    var builder = function(options){
-        this.defaultTag = 'div';
-        $.extend(this, options);
-    };
-    builder.prototype.build = function(elConfig) {
+    jmw.build = function(elConfig, defer) {
             var el, data, $el, complete = false;
-
+            elConfig = elConfig || {};
             if(elConfig.el){
                 $el = $(elConfig.el);
                 delete elConfig.el;
-            } else if (elConfig.role) {
+            }
+            if(elConfig.theme){
+                elConfig.data = elConfig.data || {};
+                elConfig.data.theme = elConfig.theme;
+                delete elConfig.theme;
+            }
+            if (elConfig.role) {
                 $el = jmw.buildWidget(elConfig.role, elConfig);
+                if(!defer || !elConfig.defer){
+                    $el.trigger('create');
+                }
                 complete = !!$el;
             } else {
                 el = document.createElement(elConfig.tag || this.defaultTag);
@@ -57,16 +74,7 @@
 
             if(!complete){
                 data = elConfig.data || {};
-                var nsData = ($.mobile && $.mobile.ns) ? 'data-' + $.mobile.ns + '-' : 'data-';
-                $.each(data, function(key, val) {
-                    //writes to the internal element / data cache
-                    //but not to the actual element unless the attribute
-                    //already exists
-                    $el.jqmData(key, val);
-                    //actually writes to the element
-                    $el.attr(nsData+key, val);
-                });
-
+                jmw._addDataAttr($el, data);
                 delete elConfig.tag;
                 delete elConfig.data;
 
@@ -105,6 +113,62 @@
 
             return $el;
         };
-    jmw.DomBuilder = builder;
-    jmw.builder = new builder();
-}(jmw));
+        jmw._addDataAttr = function($el, data){
+            var nsData = ($.mobile && $.mobile.ns) ? 'data-' + $.mobile.ns + '-' : 'data-';
+            $.each(data, function(key, val) {
+                //writes to the internal element / data cache
+                //but not to the actual element unless the attribute
+                //already exists
+                $el.jqmData(key, val);
+                //actually writes to the element
+                $el.attr(nsData+key, val);
+            });
+        };
+
+}).call(this);
+
+(function(jmw){
+    jmw.widget = {};
+    function toData(keys, config){
+        config = config || {}, keys = keys || [];
+        config.data = config.data || {};
+        $.each(config, function(key, val){
+            if(key in keys){
+                config.data[key] = val;
+                delete config[key];
+            }
+        });
+        return config;
+    }
+    function buildMarkup(keys, config, role){
+        var cfg = toData(cfgKeys,config);
+        role = role || cfg.role || cfg.data.role;
+        delete cfg.role; delete cfg.data.role;
+        $el = jmw.build(cfg, true);
+        jmw._addDataAttr($el, {'role': role});
+        return $el;
+    }
+    //TODO - controlgroup, fixed toolbar, link, listview item, select, radio, text input/area
+    var configKeys = {
+        button: ['corners','icon','iconpos','iconshadow','inline','mini','shadow'],
+        checkbox: ['mini'],
+        collapsible: ['collapsed','collapsed-icon','content-theme','expanded-icon','iconpos','inset','mini'],
+        'collapsible-set':  ['collapsed-icon','content-theme','expanded-icon','iconpos','inset','mini'],
+        dialog: ['close-btn-text', 'dom-cache', 'overlay-theme', 'title'],
+        slider: ['highlight', 'mini', 'track-theme'],
+        footer: ['id', 'position', 'fullscreen'],
+        header: ['id', 'position', 'fullscreen'],
+        listview: ['autodividers', 'count-theme', 'divider-theme', 'filter', 'filter-placeholder', 'filter-theme',
+            'header-theme', 'inset', 'split-theme', 'split-icon'],
+        navbar: ['iconpos'],
+        page: ['add-back-btn', 'back-btn-text', 'back-btn-theme', 'close-btn-text', 'dom-cache', 'overlay-theme', 'title', 'url'],
+        popup: ['corners', 'overlay-theme', 'shadow', 'tolerance']
+    };
+    $.each(configKeys, function(role,keys){
+        jmw.widget[key] = (function(){
+            return function(config){
+                return buildMarkup(keys, config, role);
+            };
+        }());
+    });
+})(jmw);
